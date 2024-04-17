d468573e569dbabf6fafa967aaa30ff3b6cd972a
loicdvl <loic.devoille@hotmail.fr>
2024-04-17 19:14:36 +0200

    add repositories

--- src/main/java/guru/springframework/spring6webapp/repositories/AuthorRepository.java
+++ src/main/java/guru/springframework/spring6webapp/repositories/AuthorRepository.java
@@ -1,2 +1,8 @@
-package guru.springframework.spring6webapp.repositories;public class AuthorRepository {
+package guru.springframework.spring6webapp.repositories;
+
+import guru.springframework.spring6webapp.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
+
